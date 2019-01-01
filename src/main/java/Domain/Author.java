33e58c84327829966dce189eec5c52bb1a045fe6
piegarden@gmail.com <40525096+piegarden1@users.noreply.github.com>
2019-01-01 20:35:59 +0100

    Vaughan trying to add the model

--- src/main/java/Domain/Author.java
+++ src/main/java/Domain/Author.java
@@ -1,6 +1,34 @@
 package Domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Author {
-    private String firstName = "";
-    private String lastName = "";
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String ID;
+    private String firstName;
+    private String lastName;
+
+    //Constructor
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+
 }
