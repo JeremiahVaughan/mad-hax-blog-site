5966f7bf1a1df65b2b3b7679bf0f2b87fbc48174
James Cathcart <cathcart.james@mad-hax.com>
2019-01-01 12:19:46 -0700

    Added JPA dependency to POM, updated Article class

--- src/main/java/Domain/Article.java
+++ src/main/java/Domain/Article.java
@@ -1,12 +1,57 @@
 package Domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.ArrayList;
 
+@Entity
 public class Article {
 
-    private long publicationDate = 0;
-    private String title = "";
-    private String body = "";
-    private ArrayList<String> comments;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private long publicationDate;
+    private String title;
+    private String body;
 
+    // Empty constructor for convenience
+    public Article() { }
+
+    // Constructor for new articles - no ID field because it wont have an ID until
+    // submitted to the database.
+    public Article(long publicationDate, String title, String body) {
+        this.publicationDate = publicationDate;
+        this.title = title;
+        this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(long publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
